4c0a4648c764c55b9d02b693c9597a112e6bfa8c
cocazeem <cocazapro@gmail.com>
2026-09-16 11:48:21 +0530

    Update test.py

--- test/test.py
+++ test/test.py
@@ -1,40 +1,41 @@
-# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
-# SPDX-License-Identifier: Apache-2.0
-
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
+from cocotb.triggers import RisingEdge
 
 
 @cocotb.test()
-async def test_project(dut):
-    dut._log.info("Start")
+async def test_reaction_timer(dut):
 
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, unit="us")
+    clock = Clock(dut.clk, 10, units="ns")
     cocotb.start_soon(clock.start())
 
-    # Reset
-    dut._log.info("Reset")
     dut.ena.value = 1
-    dut.ui_in.value = 0
-    dut.uio_in.value = 0
     dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
+    dut.ui_in.value = 0
+
+    # Reset
+    for _ in range(2):
+        await RisingEdge(dut.clk)
+
     dut.rst_n.value = 1
 
-    dut._log.info("Test project behavior")
+    # Start
+    dut.ui_in.value = 1
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    await RisingEdge(dut.clk)
 
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
+    # Release START
+    dut.ui_in.value = 0
 
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    # Count for several cycles
+    for _ in range(5):
+        await RisingEdge(dut.clk)
 
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+    # Stop
+    dut.ui_in.value = 2
+
+    await RisingEdge(dut.clk)
+
+    dut.ui_in.value = 0
+
+    print("Timer value:", int(dut.uo_out.value))
