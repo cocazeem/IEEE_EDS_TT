c04e2911f5e53c7153bd1f35798c67f10e76e97b
cocazeem <cocazapro@gmail.com>
2026-09-16 11:46:47 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -1,27 +1,50 @@
-/*
- * Copyright (c) 2024 Your Name
- * SPDX-License-Identifier: Apache-2.0
- */
-
 `default_nettype none
+`timescale 1ns / 1ps
 
-module tt_um_example (
-    input  wire [7:0] ui_in,    // Dedicated inputs
-    output wire [7:0] uo_out,   // Dedicated outputs
-    input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
-    output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
-    input  wire       clk,      // clock
-    input  wire       rst_n     // reset_n - low to reset
+module reaction_timer (
+    input  wire       clk,
+    input  wire       rst_n,
+    input  wire       ena,
+    input  wire [7:0] ui_in,
+    output reg  [7:0] uo_out,
+    input  wire [7:0] uio_in,
+    output wire [7:0] uio_out,
+    output wire [7:0] uio_oe
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+    reg running;
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+    assign uio_out = 8'b0;
+    assign uio_oe  = 8'b0;
+
+    always @(posedge clk) begin
+
+        if (!rst_n) begin
+            uo_out <= 8'd0;
+            running <= 1'b0;
+        end
+
+        else if (ena) begin
+
+            // START
+            if (ui_in[0]) begin
+                running <= 1'b1;
+            end
+
+            // STOP
+            if (ui_in[1]) begin
+                running <= 1'b0;
+            end
+
+            // Count while running
+            if (running && !ui_in[1]) begin
+                if (uo_out != 8'hFF)
+                    uo_out <= uo_out + 1'b1;
+            end
+
+        end
+    end
 
 endmodule
+
+`default_nettype wire
